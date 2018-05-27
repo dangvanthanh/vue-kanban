edccc955f04b242bcf9c443e15e9a956a60cbc8b
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-05-27 21:32:23 +0700

    First desktop app using Vuido

---
+++ README.md
@@ -0,0 +1,18 @@
+# vue-monitoring
+
+> Vue Monitoring
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# build
+npm run build
+
+# run the bundled script
+npm start
+```
+
+For detailed explanation on how things work, consult the [Vuido documentation](https://vuido.mimec.org/).
---
+++ src/components/MainWindow.vue
@@ -0,0 +1,28 @@
+<template>
+  <Window title="Vue Kanban" width="400" height="100" margined v-on:close="exit">
+    <Box horizontal padded>
+      <Text>{{ msg }}</Text>
+      <Button v-on:click="reverse">Reverse</Button>
+    </Box>
+  </Window>
+</template>
+
+<script>
+import libui from 'libui-node';
+
+export default {
+  data () {
+    return {
+      msg: 'First Desktop App Using Vuido'
+    }
+  },
+  methods: {
+    reverse () {
+      this.msg = this.msg.split('').reverse().join('');
+    },
+    exit() {
+      libui.stopLoop();
+    }
+  }
+}
+</script>
